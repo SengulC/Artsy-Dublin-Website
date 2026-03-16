14a28b2f3ff4d7dd3e75a934775788d2f576895b
Ge You <youge071@gmail.com>
2026-03-16 02:18:50 +0000

    Add CORS and posts route to the server

--- artsy-server/index.js
+++ artsy-server/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));//static path
 
 const eventsRoute = require("./routes/events");
 app.use("/events", eventsRoute);
 
 const usersRoute = require("./routes/users")
 app.use("/users", usersRoute);
+
+const postsRoute = require("./routes/posts")
+app.use("/posts", postsRoute);
 
 app.listen(3005, () => {
     console.log("Server running on http://localhost:3005")
