729d4fa66b3fc43a7d5ad17afb90033c27918fe5
SengulC <sengulcagdal@gmail.com>
2026-03-16 20:20:39 +0000

    using dbconfig!

--- artsy-server/utils/dbconfig.js
+++ artsy-server/utils/dbconfig.js
@@ -5,9 +5,6 @@ module.exports = {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,    
-    authPlugins: {
-        mysql_native_password: () => () => Buffer.from(process.env.DB_PASSWORD + '\0')
-    }
+    password: process.env.DB_PASSWORD,
     //***the naming now is different in events and users model, need to make them consistent
 }
