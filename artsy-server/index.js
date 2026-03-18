5c6d8a07f60b2393f2d4a0aaedb0bc0b7c7e07cf
astridasi <astrid1108@163.com>
2026-03-18 17:11:10 +0000

    Add files via upload

--- artsy-server/index.js
+++ artsy-server/index.js
@@ -1,30 +1,65 @@
+require("dotenv").config();
+const session = require("express-session");
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const fileUpload = require('express-fileupload');
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));//static path
-
-// must be before routes so req.files is available in controllers
-app.use(fileUpload({                                
-    limits: { fileSize: 5 * 1024 * 1024 },
-    abortOnLimit: true,
-    responseOnLimit: "File size exceeds the 5MB limit."
-})
-);
+app.use(express.static(path.join(__dirname, "public"))); //static path
 
 const eventsRoute = require("./routes/events");
 app.use("/events", eventsRoute);
 
-const usersRoute = require("./routes/users")
+const usersRoute = require("./routes/users");
 app.use("/users", usersRoute);
 
-const postsRoute = require("./routes/posts")
+//related to posts(diaries)
+const postsRoute = require("./routes/posts");
 app.use("/posts", postsRoute);
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
+
+app.get("/homepage", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/userstate.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/createUser.html"));
+});
+app.get("/userstate", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/userstate.html"));
+});
+
+app.get("/api/check-auth", (req, res) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ isLoggedIn: false, message: "No token" });
+  }
+
+  jwt.verify(
+    token,
+    process.env.JWT_SECRET || "your_jwt_secret",
+    (err, decoded) => {
+      if (err) {
+        return res
+          .status(403)
+          .json({ isLoggedIn: false, message: "Invalid token" });
+      }
+
+      res.json({
+        isLoggedIn: true,
+        userName: decoded.userName,
+      });
+    },
+  );
+});
+
 app.listen(3005, () => {
-    console.log("Server running on http://localhost:3005")
-} )
+  console.log("Server running on http://localhost:3005");
+});
