8f1a0e6d6153166608f373c09263b1298c51458e
astridasi <astrid1108@163.com>
2026-03-18 17:13:52 +0000

    Add files via upload

---
+++ artsy-server/routes/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+exports.checkAuth = (req, res) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.json({ isLoggedIn: false, message: "missing Token" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.json({ isLoggedIn: false, message: "invalid Token" });
+    }
+
+    res.json({
+      isLoggedIn: true,
+      username: decoded.username,
+    });
+  });
+};
--- artsy-server/routes/users.js
+++ artsy-server/routes/users.js
@@ -4,7 +4,12 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
 
-router.get('/:username', usersController.getUserByName);
-router.get('/',usersController.getUsersPool);
+router.get("/:username", usersController.getUserByName);
+router.get("/", usersController.getUsersPool);
+
+//router.delete('/:username',usersController.deleteUser);
+router.post("/register", usersController.createUser);
+//userlogin
+router.post("/login", usersController.loginUser);
 
 module.exports = router;
