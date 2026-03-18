4294b97bea2c252f98ba4b93b59a70584e0bcfac
mikavak1 <mikavak@tcd.ie>
2026-03-18 13:38:08 +0000

    Update events.js

--- artsy-server/models/events.js
+++ artsy-server/models/events.js
@@ -5,14 +5,10 @@ const dotenv = require('dotenv').config({path: path.join(__dirname, '..', '.env'
 const axios = require('axios');
 
 // use mysql2 pool instead of node-querybuilder bc its very buggy with insert ignore into etc..
+const dbconfig = require("../utils/dbconfig");
 const mysql2 = require("mysql2");
 // still one pool shared across all methods. not recreated per request
-const pool = mysql2.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-}).promise();
+const pool = mysql2.createPool(dbconfig).promise();
 
 // get all events
 async function get() {
