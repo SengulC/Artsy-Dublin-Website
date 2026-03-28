f14eb0063a1d3a55b470da223490138704529c48
astridasi <astrid1108@163.com>
2026-03-28 13:15:42 +0000

    Add routes for user profile, posts, attended events, stats and journal
    Add routes for user profile, posts, attended events, stats and journal

--- artsy-server/routes/users.js
+++ artsy-server/routes/users.js
@@ -4,8 +4,12 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
 
-router.get("/:username", usersController.getUserByName);
 router.get("/", usersController.getUsersPool);
+router.get("/:username", usersController.getUserByName);
+router.get("/:username/posts", usersController.getUserPosts);
+router.get("/:username/attended-events", usersController.getUserAttendedEvents);
+router.get("/:username/stats", usersController.getUserStats);
+router.get("/:username/journal", usersController.getUserJournal);
 
 //router.delete('/:username',usersController.deleteUser);
 router.post("/register", usersController.createUser);
